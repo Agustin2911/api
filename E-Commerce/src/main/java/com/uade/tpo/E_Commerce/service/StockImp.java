d6c2a7e636568c143bbbb3d9a63996917c15ff6e
Agustín Romero <80115051+Agustin2911@users.noreply.github.com>
2025-04-20 17:26:47 -0300

    Add files via upload

---
+++ E-Commerce/src/main/java/com/uade/tpo/E_Commerce/service/StockImp.java
@@ -0,0 +1,29 @@
+package com.uade.tpo.E_Commerce.service;
+
+import com.uade.tpo.E_Commerce.entity.Product_Stock;
+import com.uade.tpo.E_Commerce.entity.Shop_Stock;
+import com.uade.tpo.E_Commerce.entity.dto.CreateProductStockDTO;
+import com.uade.tpo.E_Commerce.entity.dto.ModifyStockResponse;
+
+import javax.swing.text.html.Option;
+import java.util.ArrayList;
+import java.util.Optional;
+
+public interface StockImp {
+
+    public Optional<Product_Stock> getStockOfAProductById(long id);
+
+    public Optional<CreateProductStockDTO> createProduct_Stock(long id, int stock_entry, long shop, int stock_warning);
+
+    public Optional<ArrayList<Shop_Stock>> getTheLocationOfTheProduct(long id);
+
+    public Optional<Shop_Stock> createShopStock(long id_product,int stock,long id_shop);
+
+    public Optional<ModifyStockResponse> modifyStock(long id,long id_shop, int new_stock);
+
+    public Optional<Product_Stock> modifyStock_warning(long id,int Stock_warning);
+
+
+    
+}
+
---
+++ E-Commerce/src/main/java/com/uade/tpo/E_Commerce/service/StockService.java
@@ -0,0 +1,147 @@
+package com.uade.tpo.E_Commerce.service;
+
+import com.uade.tpo.E_Commerce.entity.Product_Stock;
+import com.uade.tpo.E_Commerce.entity.Shop_Stock;
+import com.uade.tpo.E_Commerce.entity.dto.CreateProductStockDTO;
+import com.uade.tpo.E_Commerce.entity.dto.ModifyStockResponse;
+import com.uade.tpo.E_Commerce.repository.Product_StockRepository;
+import com.uade.tpo.E_Commerce.repository.Shop_StockRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import java.util.ArrayList;
+import java.util.Optional;
+
+@Service
+public class StockService implements  StockImp{
+
+    @Autowired
+    private Product_StockRepository repository1;
+
+    @Autowired
+    private Shop_StockRepository repository2;
+
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public Optional<Product_Stock> getStockOfAProductById(long id) {
+        Optional<Product_Stock> product=repository1.searchStock(id);
+
+        if (product.isPresent()){
+            return product;
+        }
+
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<CreateProductStockDTO> createProduct_Stock(long id, int stock_entry, long shop , int stock_warning) {
+
+        Optional<Product_Stock> product=repository1.searchStock(id);
+        Optional<Shop_Stock> shop_exist = repository2.searchByProductIdAndShopId(id,shop);
+
+        if(product.isPresent() && shop_exist.isPresent()){
+            return  Optional.of(new CreateProductStockDTO(product.get().getId(),product.get().getStock(),shop_exist.get().getId().getId_shop(),product.get().getStock_warning()));
+        }
+
+
+        repository1.CreateStock(id, stock_entry, stock_warning);
+        repository2.createShop_stock(id, shop, stock_entry);
+
+        product=repository1.searchStock(id);
+        shop_exist=repository2.searchByProductIdAndShopId(id,shop);
+
+        if(product.isPresent() && shop_exist.isPresent()) {
+            return  Optional.of(new CreateProductStockDTO(product.get().getId(),product.get().getStock(),shop_exist.get().getId().getId_shop(),product.get().getStock_warning()));
+        }
+
+        else {
+            return Optional.empty();
+        }
+    }
+
+
+    public Optional<ArrayList<Shop_Stock>> getTheLocationOfTheProduct(long id) {
+
+        Optional<Product_Stock> product= repository1.searchStock((id));
+        if(!product.isPresent()){
+            return Optional.empty();
+        }
+
+        else {
+            return repository2.productLocation(id);
+
+        }
+
+    }
+
+    @Override
+    public Optional<Shop_Stock> createShopStock(long id_product, int stock, long id_shop) {
+
+        Optional<Shop_Stock> shop=repository2.searchByProductIdAndShopId(id_product,id_shop);
+
+        if(shop.isPresent()){
+            return shop;
+        }
+
+        else{
+            repository2.createShop_stock(id_product,id_shop,stock);
+
+            Optional<Product_Stock> product=repository1.searchStock(id_product);
+            int new_amount=product.get().getStock()+stock;
+            repository1.modifyStock(id_product,new_amount);
+
+            return repository2.searchByProductIdAndShopId(id_product,id_shop);
+        }
+
+
+    }
+
+
+    //se introduce +5 en new stock si hay 5 nuevas unidades o -5 si salieron 5 unidades
+    @Override
+    public Optional<ModifyStockResponse> modifyStock(long id, long id_shop, int new_stock) {
+
+        Optional<Product_Stock> stock=repository1.searchStock(id);
+        Optional<Shop_Stock> shop=repository2.searchByProductIdAndShopId(id,id_shop);
+
+        if(!stock.isPresent() || !shop.isPresent()){
+            return Optional.empty();
+        }
+
+        else{
+
+
+            repository1.modifyStock(id,stock.get().getStock()+new_stock);
+            repository2.updateShop_stock(id,id_shop,shop.get().getStock()+new_stock);
+            entityManager.flush();
+            stock=repository1.searchStock(id);
+            shop=repository2.searchByProductIdAndShopId(id,id_shop);
+
+            return Optional.of(new ModifyStockResponse(id,stock.get().getStock(),id_shop,shop.get().getStock()));
+
+        }
+
+    }
+
+    @Override
+    public Optional<Product_Stock> modifyStock_warning(long id, int Stock_warning) {
+
+        Optional<Product_Stock> product=repository1.searchStock(id);
+        if(product.isPresent()){
+            repository1.modifyStockWarning(id,Stock_warning);
+            entityManager.flush();
+            product=repository1.searchStock(id);
+            return product;
+        }
+        else {
+            return Optional.empty();
+        }
+
+    }
+
+
+}
