61642b9de5e6599ecd4488dcd6c3a687347b424a
Agustín Romero <80115051+Agustin2911@users.noreply.github.com>
2025-04-20 18:40:01 -0300

    Update Basic_UserController.java

--- E-Commerce/src/main/java/com/uade/tpo/E_Commerce/controller/Basic_UserController.java
+++ E-Commerce/src/main/java/com/uade/tpo/E_Commerce/controller/Basic_UserController.java
@@ -1,4 +1,4 @@
-package com.uade.tpo.E_Commerce.controller;
+package com.uade.tpo.E_Commerce.controllers;
 
 import com.uade.tpo.E_Commerce.entity.Basic_User;
 import com.uade.tpo.E_Commerce.entity.Product;
