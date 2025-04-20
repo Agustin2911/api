40434e2d3d6229568ca425ccbaf1dc8645381b07
Agustín Romero <80115051+Agustin2911@users.noreply.github.com>
2025-04-20 18:38:28 -0300

    Update Seller_UserController.java

--- E-Commerce/src/main/java/com/uade/tpo/E_Commerce/controller/Seller_UserController.java
+++ E-Commerce/src/main/java/com/uade/tpo/E_Commerce/controller/Seller_UserController.java
@@ -1,4 +1,4 @@
-package com.uade.tpo.E_Commerce.controller;
+package com.uade.tpo.E_Commerce.controllers;
 
 import com.uade.tpo.E_Commerce.entity.Seller_User;
 import com.uade.tpo.E_Commerce.entity.dto.FailedResponse;
@@ -77,3 +77,4 @@ public class Seller_UserController {
         }
     }
 }
+
