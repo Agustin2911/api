b1c4ae6647120ffc4809fee6c43a82e42274f376
Agustin2911 <agustinezequielromer2004@gmail.com>
2025-04-22 00:19:29 -0300

    filtros

--- E-Commerce/src/main/java/com/uade/tpo/E_Commerce/controllers/config/SecurityConfig.java
+++ E-Commerce/src/main/java/com/uade/tpo/E_Commerce/controllers/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.uade.tpo.E_Commerce.controllers.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -35,7 +36,51 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req -> req.requestMatchers("/api/v1/auth/**").permitAll()
                         .requestMatchers("/error/**").permitAll()
-                        .requestMatchers("/shops/**").hasAnyAuthority(Role.ADMIN.name())
+                        .requestMatchers(HttpMethod.GET,"/basic_users/").hasAnyAuthority(Role.ADMIN.name())
+                        .requestMatchers(HttpMethod.DELETE,"/basic_users/{id}").hasAnyAuthority(Role.ADMIN.name())
+                        .requestMatchers(HttpMethod.GET,"/buyer_user").hasAnyAuthority(Role.ADMIN.name())
+                        .requestMatchers(HttpMethod.POST,"/buyer_user").permitAll()
+                        .requestMatchers(HttpMethod.PUT,"/buyer_user").hasAnyAuthority(Role.BUYER.name())
+                        .requestMatchers(HttpMethod.DELETE,"/buyer_user").hasAnyAuthority(Role.ADMIN.name())
+                        .requestMatchers(HttpMethod.GET,"/categories").permitAll()
+                        .requestMatchers(HttpMethod.GET,"/categories/{id}").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/categories").hasAnyAuthority(Role.ADMIN.name())
+                        .requestMatchers(HttpMethod.DELETE,"/categories/{id_category}").hasAnyAuthority(Role.ADMIN.name())
+                        .requestMatchers(HttpMethod.GET,"/delivery-status/{id_delivery}").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/delivery-status").hasAnyAuthority(Role.BUYER.name())
+                        .requestMatchers(HttpMethod.PUT,"/delivery-status/{id_delivery}").hasAnyAuthority(Role.SELLER.name())
+                        .requestMatchers(HttpMethod.DELETE,"/delivery-status/{id_delivery}").hasAnyAuthority(Role.SELLER.name())
+                        .requestMatchers(HttpMethod.DELETE,"/delivery-status/{id_delivery}").hasAnyAuthority(Role.ADMIN.name())
+                        .requestMatchers(HttpMethod.GET,
+                                "/product",
+                                "/product/productById/{id}",
+                                "/product/byCategoryid/{id}",
+                                "/product/bySubCategoryid/{id}"
+                        ).permitAll()
+                        .requestMatchers(HttpMethod.POST,"/product").hasAnyAuthority(Role.SELLER.name())
+                        .requestMatchers(HttpMethod.PUT,"/product").hasAnyAuthority(Role.SELLER.name())
+                        .requestMatchers(HttpMethod.DELETE,"/product/{id}").hasAnyAuthority(Role.SELLER.name())
+                        .requestMatchers(HttpMethod.DELETE,"/product/{id}").hasAnyAuthority(Role.ADMIN.name())
+                        .requestMatchers(HttpMethod.GET,"/review/{id_product}").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/review").permitAll()
+                        .requestMatchers(HttpMethod.GET,"/roles").hasAnyAuthority(Role.ADMIN.name())
+                        .requestMatchers(HttpMethod.GET,"/roles/{id}").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/roles").permitAll()
+                        .requestMatchers(HttpMethod.PUT,"/roles").hasAnyAuthority(Role.ADMIN.name())
+                        .requestMatchers(HttpMethod.DELETE,"/roles/{id}").hasAnyAuthority(Role.ADMIN.name())
+                        .requestMatchers(HttpMethod.GET,"/roles/user/{userId}").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/roles/assign/{roleId}/{userId}").permitAll()
+                        .requestMatchers(HttpMethod.DELETE,"/roles/user/{userId}").hasAnyAuthority(Role.ADMIN.name())
+                        .requestMatchers(HttpMethod.GET,"/shops/all/{id}").permitAll()
+                        .requestMatchers(HttpMethod.GET,"/shops/{id}").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/shops").hasAnyAuthority(Role.SELLER.name())
+                        .requestMatchers(HttpMethod.PUT,"/shops").hasAnyAuthority(Role.SELLER.name())
+                        .requestMatchers(HttpMethod.DELETE,"/shops/{id}").hasAnyAuthority(Role.SELLER.name())
+                        .requestMatchers(HttpMethod.GET,"/stock/getLocation/{id}").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/stock").hasAnyAuthority(Role.SELLER.name())
+                        .requestMatchers(HttpMethod.POST,"/stock/shop_stock").hasAnyAuthority(Role.SELLER.name())
+                        .requestMatchers(HttpMethod.PUT,"/stock").permitAll()
+                        .requestMatchers(HttpMethod.PUT,"/stock/stockWarning").hasAnyAuthority(Role.SELLER.name())
                         .anyRequest()
                         .authenticated())
                 .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
