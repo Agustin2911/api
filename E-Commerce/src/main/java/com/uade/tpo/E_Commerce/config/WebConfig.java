53b6b8f66d407131e64dddc0d28a4de09d8899a1
Tomas Gorrini Rech <tgorrinirech@gmail.com>
2025-06-02 00:36:55 -0300

    agrega archivo de webConfig para imagenes y modifica autorizacion de Get stock

---
+++ E-Commerce/src/main/java/com/uade/tpo/E_Commerce/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.uade.tpo.E_Commerce.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Aquí mapeamos la URL /images/** a la carpeta donde están las imágenes
+        registry.addResourceHandler("/images/products/**")
+                .addResourceLocations("file:C:/Users/Tomas/Escritorio/tpo_apis/api/E-Commerce/src/main/java" +
+                        "/com/uade/tpo/E_Commerce/images/products/");
+    }
+}
+
